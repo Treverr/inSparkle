36fa7338bbe6478b892bf0303d4647c688935df6
Trever Chambers Ewing <treverewing@me.com>
2015-12-12 00:55:53 -0500

    Set Up customer lookup screen and Swift file

---
+++ inSparkle/CustomerLookupTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomerLookupTableViewController.swift
+//  inSparkle
+//
+//  Created by Trever on 12/12/15.
+//  Copyright © 2015 Sparkle Pools. All rights reserved.
+//
+
+import UIKit
+
+class CustomerLookupTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
