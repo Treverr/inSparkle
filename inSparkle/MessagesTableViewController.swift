8b7905bbcadb6d7c266a546abdcec5320e5f0e7d
Trever <trever@trever.me>
2015-12-23 01:23:44 -0500

    Cell Set up. Waiting for design approval.  - Messages Screen Design / Set Up #70

---
+++ inSparkle/MessagesTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MessagesTableViewController.swift
+//  inSparkle
+//
+//  Created by Trever on 12/23/15.
+//  Copyright © 2015 Sparkle Pools. All rights reserved.
+//
+
+import UIKit
+
+class MessagesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationbar()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("messageCell") as! MessagesMainTableViewCell
+        
+        cell.configureCell("Trever Ewing", date: NSDate(), messageStatus: "Respond", unread: true)
+        
+        return cell
+        
+    }
+    
+    func setupNavigationbar()  {
+        self.navigationController?.navigationBar.barTintColor = Colors.sparkleBlue
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
+    }
+
+}
