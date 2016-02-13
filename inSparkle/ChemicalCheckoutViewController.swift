e21ef8c9ce003bbac89614d4698b6fe435cbaaa8
Trever <trever@trever.me>
2016-02-13 16:15:50 -0500

    Added List, Updated cart icon - Chemical Checkout Screen #34

--- inSparkle/ChemicalCheckoutViewController.swift
+++ inSparkle/ChemicalCheckoutViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 import iCarousel
 
-class ChemicalCheckoutViewController: UIViewController {
+class ChemicalCheckoutViewController: UIViewController, iCarouselDelegate, iCarouselDataSource {
 
-    @IBOutlet var carouselOutlet: iCarousel!
+    @IBOutlet var carousel : iCarousel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        carousel.type = .CoverFlow2
+        carousel.delegate = self
+        carousel.dataSource = self
         
     }
 
@@ -30,35 +34,55 @@ class ChemicalCheckoutViewController: UIViewController {
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.LandscapeLeft
     }
-
-}
-
-extension ChemicalCheckoutViewController : iCarouselDelegate, iCarouselDataSource {
+    
     
     func numberOfItemsInCarousel(carousel: iCarousel) -> Int {
-        return 1
+        return ChemicalsData.checmicalNames.count
     }
     
     func carousel(carousel: iCarousel, viewForItemAtIndex index: Int, reusingView view: UIView?) -> UIView {
         
         var itemView : UIImageView!
+        var label : UILabel!
+        
+        let itemNames = ChemicalsData.checmicalNames
         
         if (view == nil) {
-            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+            itemView.backgroundColor = UIColor.whiteColor()
+            itemView.layer.cornerRadius = 8.0
+            itemView.clipsToBounds = true
             itemView.contentMode = .ScaleAspectFit
+            
+            label = UILabel(frame: itemView.frame)
+            label.backgroundColor = UIColor.clearColor()
+            label.textColor = UIColor.blackColor()
+            label.textAlignment = .Center
+            label.font = label.font.fontWithSize(50)
+            label.adjustsFontSizeToFitWidth = true
+            label.tag = 1
+            itemView.addSubview(label)
         } else {
             itemView = view as! UIImageView
+            label = itemView.viewWithTag(1) as! UILabel!
         }
         
-        itemView.image = UIImage(named: "ITEM AT INDEX")
+        label.text = itemNames[index]
         
         return itemView
     }
     
     func carousel(carousel: iCarousel, didSelectItemAtIndex index: Int) {
-        let selectedIndex = index
+        
+        let alert = UIAlertController(title: "Added", message: nil, preferredStyle: .Alert)
+        self.presentViewController(alert, animated: true, completion: nil)
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.3 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+
         
     }
-    
-    
+
+
 }
---
+++ inSparkle/ChemicalData.swift
@@ -0,0 +1,72 @@
+//
+//  ChemicalColors.swift
+//  inSparkle
+//
+//  Created by Trever on 2/12/16.
+//  Copyright © 2016 Sparkle Pools. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ChemicalColors {
+    
+}
+
+class ChemicalsData : NSObject {
+    
+    static let checmicalNames : [String] = ["Algae Hunter",
+                                            "Algaecide 30",
+                                            "Algicil ",
+                                            "5lb Alkalinity Rise",
+                                            "9lb Alkalinity Rise",
+                                            "25lb Alkalinity Rise",
+                                            "Alum",
+                                            "Ascorb-X",
+                                            "5lb Chlorinating Granules",
+                                            "10lb Chlorinating Granules",
+                                            "25lb Chlorinating Granules",
+                                            "50lb Chlorinating Granules",
+                                            "2lb Chlorine Stabilizer",
+                                            "4lb Chlorine Stabilizer",
+                                            "5lb 3-inch Chlorine Tablets",
+                                            "10lb 3-inch Chlorine Tablets",
+                                            "25lb 3-inch Chlorine Tablets",
+                                            "50lb 3-inch Chlorine Tablets",
+                                            "5lb 1-inch Chlorine Tablets",
+                                            "10lb 1-inch Chlorine Tablets",
+                                            "25lb 1-inch Chlorine Tablets",
+                                            "Crystal Cube",
+                                            "CULator",
+                                            "Diamond Blue",
+                                            "10lb DE (Earth)",
+                                            "25lb DE (Earth)",
+                                            "50lb DE (Earth)",
+                                            "Hydro-Floc",
+                                            "1 Gallon Liquid Chlorine",
+                                            "1 Case Liquid Chlorine",
+                                            "Majestic Blue",
+                                            "1 Gallon Muriatic Acid",
+                                            "1 Case Muriatic Acid",
+                                            "Novatrol",
+                                            "2lb Pepper",
+                                            "3lb pH Lower",
+                                            "6lb pH Lower",
+                                            "2.5lb pH Rise",
+                                            "5lb pH Rise",
+                                            "9lb pH Rise",
+                                            "25lb pH Rise",
+                                            "50lb pH Rise",
+                                            "Phos-X",
+                                            "PoolRx (Black)",
+                                            "PoolRx (Blue)",
+                                            "QLF Algaecide",
+                                            "1lb Quick Shock",
+                                            "5lb Rapid Shock",
+                                            "10lb Rapid Shock",
+                                            "25lb Rapid Shock",
+                                            "40lb Salt",
+                                            "50lb Sand",
+                                            "Stain & Scale Control",
+                                            "Stain-X",]
+}
