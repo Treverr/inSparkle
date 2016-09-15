573dd3baf24d4b03736ed34840716e9fa207a244
Trever <Trever@trevers-imac>
2016-09-14 22:41:26 -0400

    Minor code updates - Emails #13

--- inSparkle/CloudCode.swift
+++ inSparkle/CloudCode.swift
@@ -14,12 +14,9 @@ class CloudCode {
     
     class func AddUpdateCustomerRecord(accountNumber : String?, name : String, address : String, phoneNumber : String) {
         
-        let mgKey = "key-c2853cd746036f9c43ae5ba6bdbc8e7a"
-        let mgDomain = "mysparklepools.com"
-        
         if accountNumber != nil {
             
-            var body = "The following customer needs added or updated: \n\n Customer Account Number: \(accountNumber!)\n Customer Name: \(name)\n Customer Address: \(address)\n Customer Phone Number: \(phoneNumber)\n\n Thank you,\n\n The inSparkle App Team\n\n\n\n*** This is an automatically generated email, please do not reply ***"
+            let body = "The following customer needs added or updated: \n\n Customer Account Number: \(accountNumber!)\n Customer Name: \(name)\n Customer Address: \(address)\n Customer Phone Number: \(phoneNumber)\n\n Thank you,\n\n The inSparkle App Team\n\n\n\n*** This is an automatically generated email, please do not reply ***"
             
             let parameters = [
                 "to" : "robert@mysparklepools.com",
@@ -32,7 +29,7 @@ class CloudCode {
             
         } else {
             
-            var body = "The following customer needs added or updated: \n\n Customer Account Number:\n Customer Name: \(name)\n Customer Address: \(address)\n Customer Phone Number: \(phoneNumber)\n\n Thank you,\n\n The inSparkle App Team\n\n\n\n*** This is an automatically generated email, please do not reply ***"
+            let body = "The following customer needs added or updated: \n\n Customer Account Number:\n Customer Name: \(name)\n Customer Address: \(address)\n Customer Phone Number: \(phoneNumber)\n\n Thank you,\n\n The inSparkle App Team\n\n\n\n*** This is an automatically generated email, please do not reply ***"
             
             let parameters = [
                 "to" : "robert@mysparklepools.com",
